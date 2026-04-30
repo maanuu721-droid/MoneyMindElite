41055b1d3ae213156f6f71c8ff09c22befeda9e8
Bahia Go User <bahia.go@example.com>
2026-04-30 07:32:24 -0600

    fix: Revert config and ensure public folder is tracked

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  /* config options here */
 };
 
 export default nextConfig;
