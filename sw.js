d86c0af74d653ac9b94778d4f4e2bafa6a373d69
maanuu721-droid <maanuu721@gmail.com>
2026-05-11 18:08:36 -0600

    Restore Monetag Multitag ID 238478 in index.html and sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "moneymindelite.com",
-    "zoneId": 10994514
+    "zoneId": 238478
 }
 self.lary = ""
 importScripts('https://quge5.com/act/files/service-worker.min.js?r=sw')
